eb61ce607b7593d5a3349bd0177f8a43916e2fb7
Damzxyno <decagon@Decagons-MacBook-Air.local>
2022-08-02 15:41:17 +0100

    Initial Commit

---
+++ RabbitMQ_Learn/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ_Learn.Producer;
+
+public class Program
+{
+    public Program()
+    {
+        
+    }
+
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() 
+        {
+             Uri = new Uri("amqp://guest:guest@localhost:5672")
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channelModel = connection.CreateModel();
+
+        channelModel.QueueDeclare(
+            "demo-queue", 
+            durable: true, 
+            exclusive: false, 
+            autoDelete: false,
+            arguments: null );
+        
+        var message = new {Name= "Damilola", Message = "Hello!!"};
+        var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+        channelModel .BasicPublish("", "demo-queue", null, body);
+
+    }
+}
