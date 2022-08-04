83f08d9362441146d4c0a708cc887ec444e812e7
Damzxyno <decagon@Decagons-MacBook-Air.local>
2022-08-04 12:20:00 +0100

    Added Java to C# code to test rabbit-mq language agnosy

---
+++ src/main/java/com/damzxyno/javarabbitmq/JavaRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.damzxyno.javarabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRabbitMqApplication.class, args);
+    }
+
+}
